691b80702774923ac23feceb1a32cd5cf31667a4
kris koshel <kristina.koschel@mail.ru>
2022-11-27 20:47:52 +1000

    AuthenticationForm front

--- WebApplication1/client/src/components/AuthenticationForm.tsx
+++ WebApplication1/client/src/components/AuthenticationForm.tsx
@@ -6,6 +6,12 @@ import { Typography } from '@mui/material';
 import AuthInput from "../objects/AuthInput";
 import React from "react";
 import authRepository from "../repositories/AuthRepository";
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 const AuthenticationForm = () => {
@@ -28,43 +34,78 @@ const AuthenticationForm = () => {
             });
     }
 
+    const theme = createTheme();
 
     return (
-        <Box
-            sx={{
-                width: 550,
-                height: 350,
-                backgroundColor: "white",
-                borderRadius: "15px"
-            }}
-        >
-            <Grid
-                sx={{
-                    width: 550,
-                    height: 350
-                }}
-                container
-                spacing={2}
-                direction="column"
-                alignItems="center"
-                justifyContent="center">
-                <Grid item>
-                    <Typography variant="h5">
-                        Authentication
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
                     </Typography>
-                </Grid>
-                <Grid item width={300}>
-                    <TextField id="standard-basic" label="Login" variant="standard" fullWidth inputRef={loginRef} />
-                </Grid>
-                <Grid item width={300}>
-                    <TextField id="standard-basic" label="Password" variant="standard" fullWidth type="password" inputRef={passwordRef} />
-                </Grid>
-                <Grid item>
-                    <Button onClick={handleLogin} variant="contained">Log in</Button>
-                </Grid>
-            </Grid>
-        </Box>
-    )
+                    <Box>
+
+                        <TextField
+                            id="standard-basic"
+                            label="Login"
+                            variant="standard"
+                            fullWidth
+                            inputRef={loginRef}
+                            margin="normal"
+                            required
+                            name="login"
+                            autoComplete="login"
+                            autoFocus
+                        />
+                        <TextField
+                            id="standard-basic"
+                            label="Password"
+                            variant="standard"
+                            fullWidth type="password"
+                            inputRef={passwordRef}
+                            margin="normal"
+                            required
+                            name="password"
+                            autoComplete="current-password"
+                        />
+
+                        <Button
+                            onClick={handleLogin}
+                            variant="contained"
+                            type="submit"
+                            fullWidth
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default AuthenticationForm;
